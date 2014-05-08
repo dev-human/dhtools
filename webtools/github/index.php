725a9c2bd864c11b4d81a44b08401fd801ae2d7e
Erika Heidi <erika.christina@gmail.com>
2014-05-08 15:05:34 +0200

    hook script now working

--- webtools/github/index.php
+++ webtools/github/index.php
@@ -3,6 +3,7 @@
 require_once __DIR__ . '/../../vendor/autoload.php';
 
 $app = new Silex\Application();
+$app['debug'] = true;
 
 $app->get('/', function () use ($app) {
     return 'Nothing to see here.';
@@ -14,6 +15,21 @@ $app->get('/event/push', function () use ($app) {
      * 1 - cd to /dev-human/dev-human and pull from main repo
      * 2 - run the sculpin command and generate the prod output
      */
+
+    $local_path = "/www/dev-human/dev-human";
+    //$local_path = "/home/erika/Projects/dev-human";
+    $update_log = __DIR__ . '/../../logs/update.log';
+
+    shell_exec("cd $local_path && git pull 2>&1");
+    shell_exec("cd $local_path && sculpin generate --env=prod 2>&1");
+
+    $fp = fopen($update_log, "a+");
+    if ($fp !== false) {
+        fwrite($fp, date('[Y-m-d H:i:s]') . " Updated production website.\n");
+        fclose($fp);
+    }
+
+    return new \Symfony\Component\HttpFoundation\Response("Done.");
 });
 
 $app->run();
